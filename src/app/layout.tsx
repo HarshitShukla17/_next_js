b1e8bd3f5af773dccd34040d426b4af8d1b6c226
Harshit <hs4713513@gmail.com>
2024-10-11 21:12:07 +0530

    added the font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,15 +7,52 @@ import {
 } from '@clerk/nextjs'
 import './globals.css'
 import React from 'react'
+import {Inter,Space_Grotesk} from 'next/font/google'
+import { Metadata } from 'next'
+
+export const meta: Metadata = {
+  title: 'DevFlow',
+  description: 'Ask a question and get answers from the community',
+  icons: {
+    icon: '/assets/images/site-logo.svg',
+  }
+}
+
+const inter=Inter(
+  {
+    subsets: ['latin'],
+    weight:['100','200','300','400','500','600','700','800','900'],
+    variable:'--font-inter'
+  }
+)
+
+const spaceGrotesk=Space_Grotesk(
+  {
+    subsets: ['latin'],
+    weight:['300','400','500','600','700'],
+    variable:'--font-space-grotesk'
+  }
+)
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={
+        {
+          elements:{
+            formButtonPrimary:'primary-gradient',
+            footerActionLink:'primary-text-gradient hover:text-primary-500',
+          }
+        }
+      }
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk}`}>
+          
           <SignedOut>
             <SignInButton />
           </SignedOut>
